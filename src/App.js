21b81767dc53978d673adf7879a14249e15ac680
Carlos <happysmile000916@gmail.com>
2023-09-19 18:54:30 -0700

    testing-third

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload123123123123123123123.
+          Welcome!
         </p>
       </header>
     </div>
